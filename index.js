31ea98eb36356f40d3af80a69053b00c7a5ea982
phamdai <daipham952@gmail.com>
2022-06-21 18:12:46 +0700

    make it better

--- index.js
+++ index.js
@@ -16,6 +16,12 @@ let users = [];
 const addUser = (user, socketId) => {
   !users.some((item) => item.user.id === user.id) &&
     users.push({ user, socketId });
+users=users.map(item=>{
+    if(item.socketId==socketId){
+      return({...item,user:{...item.user,online:true}})
+    }
+    return({...item})
+  })
 };
 
 const removeUser = (socketId) => {
